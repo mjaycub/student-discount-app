88042011836933e864a8a79413bbade090092394
Mark Jacob <mark.jacob@sap.com>
2019-09-03 01:22:42 +0100

    Add basic links to home && redirect page && header styling
    - Added basic links to home and college reg page for demo info
    - Improved header styling format

--- src/CollegeRegistration.js
+++ src/CollegeRegistration.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class CollegeRegistration extends Component {
   render() {
     const CollegeRegistration = () => (
-      <div>
-        <h1>registration page</h1>
+       <div>
+        <h1>Registration page</h1>
+        <p>Empty for this demo. This would be a registration/request form for specific colleges. See links below for demo:</p>
+        <ul>
+          <li><a href="/college/usc">/college/usc</a></li>
+          <li><a href="/admin">/admin</a></li>
+        </ul>
       </div>
     )
     return (
--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,12 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>home page</h1>
+        <h1>Home page</h1>
+        <p>Empty for this demo. This would be the usual business landing page. See links below:</p>
+        <ul>
+          <li><a href="/college/usc">/college/usc</a></li>
+          <li><a href="/admin">/admin</a></li>
+        </ul>
       </div>
     );
   }
